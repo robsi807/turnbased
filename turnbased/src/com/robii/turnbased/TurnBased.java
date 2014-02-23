683228029c074e6c4a74d2c660e84d4652d28ee0
robii <robinsilverhav@gmail.com>
2014-02-23 19:41:20 +0100

    added fontmanager

--- turnbased/src/com/robii/turnbased/TurnBased.java
+++ turnbased/src/com/robii/turnbased/TurnBased.java
@@ -2,12 +2,14 @@ package com.robii.turnbased;
 
 import com.badlogic.gdx.Game;
 import com.robii.turnbased.gfx.TextureHandler;
+import com.robii.turnbased.input.FontHandler;
 
 public class TurnBased extends Game {
 
 	@Override
 	public void create() {
 		TextureHandler.init();
+		FontHandler.init();
 		setScreen(new GameScreen());
 	}
 
---
+++ turnbased/src/com/robii/turnbased/input/FontHandler.java
@@ -0,0 +1,20 @@
+package com.robii.turnbased.input;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+
+public class FontHandler {
+
+	public static BitmapFont[] font;
+
+	public static void init() {
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(
+				Gdx.files.internal("fonts/pixel.ttf"));
+		font = new BitmapFont[20];
+		for (int x = 0; x < 20; x++)
+			font[x] = generator.generateFont((x + 1) * 10);
+		generator.dispose();
+	}
+
+}
